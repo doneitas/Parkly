33ec5dd4302c5de16370e4b42860930fd4b7531c
Giedrius <Giedrius.Valnas@users.noreply.github.com>
2018-03-05 01:07:49 +0200

    License Plate Database and design hotfix

---
+++ app/src/main/java/com/example/parkly/DataBase/LocalUserDataSource.java
@@ -0,0 +1,12 @@
+package com.example.parkly.DataBase;
+
+/**
+ * Created by Giedrius on 2018-03-04.
+ */
+
+public class LocalUserDataSource {
+
+    private LicensePlateDao licensePlateDao;
+    private static LocalUserDataSource mInstance;
+
+}
