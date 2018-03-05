b781e79e42ef26eee963d91f402b84b7002e5244
Giedrius <Giedrius.Valnas@users.noreply.github.com>
2018-03-05 22:25:45 +0200

    Database in progress

--- app/src/main/java/com/example/parkly/Cars.java
+++ app/src/main/java/com/example/parkly/Cars.java
@@ -1,10 +1,11 @@
 package com.example.parkly;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -16,9 +17,12 @@ import com.example.parkly.DataBase.LocalUserDataSource;
 import java.util.ArrayList;
 import java.util.List;
 
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Action;
 import io.reactivex.functions.Consumer;
 import io.reactivex.schedulers.Schedulers;
 
@@ -26,6 +30,8 @@ public class Cars extends AppCompatActivity {
 
     Button btn_home;
     Button btn_add;
+    EditText txt_plate;
+    Button btn_confirm;
 
     private ListView lst_Car;
 
@@ -74,20 +80,61 @@ public class Cars extends AppCompatActivity {
         LicensePlateDatabase licensePlateDatabase = LicensePlateDatabase.getInstance(this);
         licensePlateRepository = LicensePlateRepository.getInstance(LocalUserDataSource.getInstance(licensePlateDatabase.licensePlateDao()));
 
-        String text = addCar.txt_plate.getText().toString();
-        if (text != null) {
-            LicensePlate licensePlate = new LicensePlate();
-            licensePlate.setNumber(text);
-            AddLicensePlate(licensePlate);
-        }
-
         loadData();
 
         init();
 
 
+        //Event
+        btn_confirm = findViewById(R.id.btn_confirm);
+        txt_plate = findViewById(R.id.txt_plate);
+        btn_confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Disposable disposable = io.reactivex.Observable.create(new ObservableOnSubscribe<Object>() {
+                    @Override
+                    public void subscribe(ObservableEmitter<Object> e) throws Exception {
+
+                        String text = txt_plate.getText().toString();
+                        if (text != null) {
+                            LicensePlate licensePlate = new LicensePlate();
+                            licensePlate.setNumber(text);
+                            licensePlateRepository.insertAll(licensePlate);
+                            e.onComplete();
+                        }
+                    }
+
+
+                })
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribeOn(Schedulers.io())
+                        .subscribe(new Consumer<Object>() {
+                                       @Override
+                                       public void accept(Object o) throws Exception {
+                                           Toast.makeText(Cars.this, "License Plate added !", Toast.LENGTH_SHORT).show();
+                                       }
+                                   }, new Consumer<Throwable>() {
+                                       @Override
+                                       public void accept(Throwable throwable) throws Exception {
+                                           Toast.makeText(Cars.this, "" + throwable.getMessage(), Toast.LENGTH_SHORT).show();
+                                       }
+                                   },
+                                new Action() {
+                                    @Override
+                                    public void run() throws Exception {
+                                        loadData(); // Refresh data
+                                    }
+                                }
+
+                        );
+
+            }
+        });
 
     }
+
+
+
 
     private void loadData()
     {
