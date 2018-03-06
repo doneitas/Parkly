e28b5a7dac1d1a6fc80eecd4b641513d4df7c472
Giedrius <Giedrius.Valnas@users.noreply.github.com>
2018-03-06 13:29:17 +0200

    Added license number check

--- app/src/main/java/com/example/parkly/Cars.java
+++ app/src/main/java/com/example/parkly/Cars.java
@@ -86,6 +86,7 @@ public class Cars extends AppCompatActivity {
                             }
 
                     );
+            compositeDisposable.add(disposable);
         }
     }
 
--- app/src/main/java/com/example/parkly/addCar.java
+++ app/src/main/java/com/example/parkly/addCar.java
@@ -7,6 +7,10 @@ import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by Marius on 2018-03-04.
@@ -38,10 +42,22 @@ public class addCar extends Activity {
             @Override
             public void onClick(View v) {
                 String value = txt_plate.getText().toString();
-                Intent myIntent = new Intent(getBaseContext(),Cars.class);
-                myIntent.putExtra("number",value);
-                startActivity(myIntent);
+                if(isNumberCorrect(value)) {
+                    Intent myIntent = new Intent(getBaseContext(), Cars.class);
+                    myIntent.putExtra("number", value);
+                    startActivity(myIntent);
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Wrong format", Toast.LENGTH_LONG).show();
+                }
             }
         });
+    }
+
+    public static boolean isNumberCorrect(String number) {
+        String expression = "[a-zA-Z]{3}+[0-9]{3}";
+        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(number);
+        return matcher.matches();
     }
 }
