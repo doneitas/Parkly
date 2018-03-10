b02828085f8bebeb31aea409cfd5994c297a8fdc
doneitas <donatas.velis@gmail.com>
2018-03-10 14:46:14 +0200

    Deleted CheckRegistration method, because he is useless. (There is a checking for txt file)

--- app/src/main/java/com/example/parkly/MainActivity.java
+++ app/src/main/java/com/example/parkly/MainActivity.java
@@ -34,7 +34,6 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
         init();
-        checkCarRegistration();
     }
 
     @Override
@@ -104,11 +103,5 @@ public class MainActivity extends AppCompatActivity
                 startActivity(new Intent(MainActivity.this, Cars.class));
             }
         });
-    }
-
-    public void checkCarRegistration(){
-        if (!getFileStreamPath("LicensePlateNumbers.txt").exists()){
-            startActivity(new Intent(MainActivity.this, addCar.class));
-        }
     }
 }
