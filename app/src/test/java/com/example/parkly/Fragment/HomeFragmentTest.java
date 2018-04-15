414c756d4a07d41557b6fa072bf827bfbcbae61d
doneitas <donatas.velis@gmail.com>
2018-04-15 14:06:53 +0300

    Made two tests. For method estimatedPrice() and calculateTimeLeft()  (both in HomeFragment.java)

--- app/src/test/java/com/example/parkly/Fragment/HomeFragmentTest.java
+++ app/src/test/java/com/example/parkly/Fragment/HomeFragmentTest.java
@@ -125,8 +125,129 @@ public class HomeFragmentTest {
     }
 
     @Test
-    public void estimatedTime() throws Exception {
-        //Paklausti
+    public void calculateTimeLeft() throws Exception {
+
+        int input1 = 15 * 60 + 15;
+        int input2 = 15;
+        int input3 = 00;
+        long output;
+        long expected = 15 * 60000;
+
+        HomeFragment homeFragment = new HomeFragment();
+        output = homeFragment.calculateTimeLeft(input1, input2, input3);
+
+        assertEquals(expected, output);
+
+        //---------------------------------------------------------------------------
+
+        input1 = (17) * 60 + (38);
+        input2 = 12;
+        input3 = 15;
+        expected = ((5) * 60 + (23)) * 60000;
+
+        homeFragment = new HomeFragment();
+        output = homeFragment.calculateTimeLeft(input1, input2, input3);
+
+        assertEquals(expected, output);
+
+        //---------------------------------------------------------------------------
+
+        input1 = (12) * 60 + (00);
+        input2 = 9;
+        input3 = 49;
+        expected = ((2) * 60 + (11)) * 60000;
+
+        homeFragment = new HomeFragment();
+        output = homeFragment.calculateTimeLeft(input1, input2, input3);
+
+        assertEquals(expected, output);
+
+        //---------------------------------------------------------------------------
+
+        input1 = (9) * 60 + (45);
+        input2 = 8;
+        input3 = 30;
+        expected = ((1) * 60 + (15)) * 60000;
+
+        homeFragment = new HomeFragment();
+        output = homeFragment.calculateTimeLeft(input1, input2, input3);
+
+        assertEquals(expected, output);
+
+        //---------------------------------------------------------------------------
+
+        input1 = (18) * 60 + (59);
+        input2 = 9;
+        input3 = 0;
+        expected = ((9) * 60 + (59)) * 60000;
+
+        homeFragment = new HomeFragment();
+        output = homeFragment.calculateTimeLeft(input1, input2, input3);
+
+        assertEquals(expected, output);
+
+        //---------------------------------------------------------------------------
+
+        input1 = (21) * 60 + (34);
+        input2 = 17;
+        input3 = 15;
+        expected = ((4) * 60 + (19)) * 60000;
+
+        homeFragment = new HomeFragment();
+        output = homeFragment.calculateTimeLeft(input1, input2, input3);
+
+        assertEquals(expected, output);
+
+        //---------------------------------------------------------------------------
+
+        input1 = (16) * 60 + (02);
+        input2 = 15;
+        input3 = 43;
+        expected = ((0) * 60 + (19)) * 60000;
+
+        homeFragment = new HomeFragment();
+        output = homeFragment.calculateTimeLeft(input1, input2, input3);
+
+        assertEquals(expected, output);
+
+        //---------------------------------------------------------------------------
+
+        input1 = (12) * 60 + (00);
+        input2 = 9;
+        input3 = 0;
+        expected = ((3) * 60 + (0)) * 60000;
+
+        homeFragment = new HomeFragment();
+        output = homeFragment.calculateTimeLeft(input1, input2, input3);
+
+        assertEquals(expected, output);
+
+        //---------------------------------------------------------------------------
+
+        input1 = (8) * 60 + (30);
+        input2 = 8;
+        input3 = 3;
+        expected = ((0) * 60 + (27)) * 60000;
+
+        homeFragment = new HomeFragment();
+        output = homeFragment.calculateTimeLeft(input1, input2, input3);
+
+        assertEquals(expected, output);
+
+        //---------------------------------------------------------------------------
+
+        input1 = (13) * 60 + (28);
+        input2 = 10;
+        input3 = 33;
+        expected = ((2) * 60 + (55)) * 60000;
+
+        homeFragment = new HomeFragment();
+        output = homeFragment.calculateTimeLeft(input1, input2, input3);
+
+        assertEquals(expected, output);
+
+        //---------------------------------------------------------------------------
+
     }
 
 
