01af66fbf4e88ac046ab8491c5b3305dd3f4d6d3
Karolis001 <Karolis001@users.noreply.github.com>
2018-04-23 20:19:47 +0300

    Fixed naming

--- app/src/main/java/com/example/parkly/Fragment/HomeFragment.java
+++ app/src/main/java/com/example/parkly/Fragment/HomeFragment.java
@@ -141,14 +141,15 @@ public class HomeFragment extends Fragment {
         compositeDisposable.add(disposable);
     }
 
+
     public void showPriceAndParkingEnding(View view)
     {
         final ArrayList<String> zones = new ArrayList<String>();
-        zones.add("Green");
-        zones.add("Blue");
-        zones.add("Red");
-        zones.add("Yellow");
-        zones.add("Orange");
+        zones.add("Green (0.30€/h)");
+        zones.add("Blue (0.60€/h)");
+        zones.add("Red (1.20€/h)");
+        zones.add("Yellow (2€/h)");
+        zones.add("Orange (2€/h)");
         final ArrayList<String> time = new ArrayList<String>();
 
 
@@ -186,7 +187,7 @@ public class HomeFragment extends Fragment {
 
 
                 switch (chosenZone){
-                    case "Green":{
+                    case "Green (0.30€/h)":{
                         time.removeAll(time);
                         time.add("1  h  0 min");
                         time.add("2  h  0 min");
@@ -201,7 +202,7 @@ public class HomeFragment extends Fragment {
                         chosenMinutes = -1;
                         break;
                     }
-                    case "Blue":{
+                    case "Blue (0.60€/h)":{
                         time.removeAll(time);
                         time.add("0  h 30 min");
                         time.add("1  h  0 min");
@@ -296,31 +297,31 @@ public class HomeFragment extends Fragment {
         double price = 0;
         switch(color)
         {
-            case "Orange":
+            case "Orange (2€/h)":
             {
                 price = 2 / 60d;
                 total = ((chosenHour*60) + chosenMinute) * price;
                 break;
             }
-            case "Yellow":
+            case "Yellow (2€/h)":
             {
                 price = 2 / 60d;
                 total = ((chosenHour*60) + chosenMinute) * price;
                 break;
             }
-            case "Blue":
+            case "Blue (0.60€/h)":
             {
                 price = 0.6 / 60d;
                 total = ((chosenHour*60) + chosenMinute) * price;
                 break;
             }
-            case "Red":
+            case "Red (1.20€/h)":
             {
                 price = 1.2 / 60d;
                 total = ((chosenHour*60) + chosenMinute) * price;
                 break;
             }
-            case "Green":
+            case "Green (0.30€/h)":
             {
                 price = 0.3 / 60d;
                 total = ((chosenHour*60) + chosenMinute) * price;
@@ -351,13 +352,14 @@ public class HomeFragment extends Fragment {
         return totalTime;
     }
 
+
     public boolean needsPopUp (String color)
     {
         Calendar currentTime = GregorianCalendar.getInstance();
         currentTime.setTime(new Date());
         switch(color)
         {
-            case "Orange":
+            case "Orange (2€/h)":
             {
                 if (currentTime.get(Calendar.HOUR_OF_DAY) >= 24 || currentTime.get(Calendar.HOUR_OF_DAY) < 8)
                 {
@@ -366,10 +368,10 @@ public class HomeFragment extends Fragment {
                 }
                 break;
             }
-            case "Green":
-            case "Blue":
-            case "Red":
-            case "Yellow":
+            case "Green (0.30€/h)":
+            case "Blue (0.60€/h)":
+            case "Red (1.20€/h)":
+            case "Yellow (2€/h)":
             {
                 if (currentTime.get(Calendar.HOUR_OF_DAY) >= 18 || currentTime.get(Calendar.HOUR_OF_DAY) <= 8)
                 {
