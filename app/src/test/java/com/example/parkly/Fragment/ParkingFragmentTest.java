917694a890a66295a2fcc22303a64c512084c680
Karolis001 <Karolis001@users.noreply.github.com>
2018-05-07 23:27:26 +0300

    Ignores multiple spaces in search box, trims extra space at start/end

---
+++ app/src/test/java/com/example/parkly/Fragment/ParkingFragmentTest.java
@@ -0,0 +1,59 @@
+package com.example.parkly.Fragment;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Karolis on 5/7/2018.
+ */
+
+public class ParkingFragmentTest {
+
+    @Test
+    public void estimatedPrice() throws Exception {
+
+        String input1 = "   Testing Space";
+        String expected = "Testing Space";
+        String output;
+
+        ParkingFragment parkingFragment = new ParkingFragment();
+        output = parkingFragment.trimText(input1);
+
+        assertEquals(expected, output);
+
+        //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+        input1 = "Testing Space          ";
+
+        output = parkingFragment.trimText(input1);
+
+        assertEquals(expected, output);
+
+        //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+        input1 = "Testing    Space";
+
+        output = parkingFragment.trimText(input1);
+
+        assertEquals(expected, output);
+
+        //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+        input1 = "       Testing    Space";
+
+        output = parkingFragment.trimText(input1);
+
+        assertEquals(expected, output);
+
+        //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+        input1 = "Testing    Space            ";
+
+        output = parkingFragment.trimText(input1);
+
+        assertEquals(expected, output);
+
+        //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    }
+}
