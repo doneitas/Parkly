cd610f0d76d23e25e97e92bd40c1c57a86ddcbdc
doneitas <donatas.velis@gmail.com>
2018-05-08 00:00:44 +0300

    merged to developer

--- app/src/main/java/com/example/parkly/Activity/MainActivity.java
+++ app/src/main/java/com/example/parkly/Activity/MainActivity.java
@@ -38,10 +38,6 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Create();
-    }
-
-    public void Create(){
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("");
