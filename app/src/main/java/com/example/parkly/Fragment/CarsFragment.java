68670912a341b07048379b4dff14abb3842d92f9
Giedrius <Giedrius.Valnas@users.noreply.github.com>
2018-05-16 21:14:28 +0300

    Added alert message after pressing delete button at cars tab when there are selected cars.

--- app/src/main/java/com/example/parkly/Fragment/CarsFragment.java
+++ app/src/main/java/com/example/parkly/Fragment/CarsFragment.java
@@ -119,7 +119,19 @@ public class CarsFragment extends Fragment {
                     {
                         lst_Car.clearFocus();
                         if(!selectedLicensePlateList.isEmpty()) {
-                            deleteSelectedLicensePlates();
+                            new AlertDialog.Builder(getActivity(), R.style.AlertDialog)
+                                    .setMessage("Do you really want to remove selected cars?")
+                                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            deleteSelectedLicensePlates();
+                                        }
+                                    }).setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            }).create().show();
                         }
                         deleteClicked = false;
                         refreshAdapter(view);
